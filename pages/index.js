e2c5ef0ca32ab51559c462989ec33973655d6788
dlatngh <sooho.lim4@gmail.com>
2023-04-10 23:41:22 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Link Tree threeeeeeeeeeeeeeeeee</title>
+        <title>Link Tree threeeeeeeeeeeeeeeeee peepee hehe</title>
         <meta name="description" content="My Link Tree" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
