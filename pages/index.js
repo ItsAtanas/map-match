2bd5f1344980749259422ebaf24f5f6b81d5e817
dlatngh <sooho.lim4@gmail.com>
2023-04-11 00:35:25 -0400

    background favicon

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Link Tree threeeeeeeeeeeeeeeeee peepee hehe</title>
-        <meta name="description" content="My Link Tree" />
+        <title>Map Match</title>
+        <meta name="description" content="Match these nuts fucker" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -16,53 +16,7 @@ export default function Home() {
         />
       </Head>
       <main>
-        <BackgroundMagic />
-        <div className="">
-          <div className="flex justify-center pt-32">
-            <img className="h-60 rounded-full" src="img/1662670355918.jpeg" />
-          </div>
-          <div className="flex justify-center mt-10">
-            <a
-              className="flex justify-center button one hover:scale-105 p-2.5 bg-transparent text-white border border-white w-1/2 rounded-xl h-11"
-              href="https://github.com/ItsAtanas"
-              target="_blank"
-            >
-              <button
-                className="flex justify-center"
-                href="https://github.com/ItsAtanas"
-              >
-                <img
-                  className="h-6 mr-2 rounded-full"
-                  src="img/github-white.png"
-                />
-                GitHub
-              </button>
-            </a>
-          </div>
-          <div className="flex justify-center mt-5">
-            <a
-              className="flex justify-center button one hover:scale-105 p-2.5 bg-transparent text-white border border-white w-1/2 rounded-xl h-11"
-              href="https://atanasbogdanoski.com/"
-              target="_blank"
-            >
-              <button className="flex justify-center">
-                <img className="h-6 mr-2 rounded-full" src="img/website.png" />
-                Portfolio
-              </button>
-            </a>
-          </div>
-          <div className="flex justify-center mt-5">
-            <a
-              className="flex justify-center button one hover:scale-105 p-2.5 bg-transparent text-white border border-white w-1/2 rounded-xl h-11"
-              href="https://www.linkedin.com/in/bogdanoski-atanas/"
-              target="_blank"
-            >
-              <button className="flex justify-center">
-                <img className="h-6 mr-2 rounded-full" src="img/linkedin.png" />
-                LinkedIn
-              </button>
-            </a>
-          </div>
+        <div className="background bg-gradient-to-tl from-[#1E1E1E] to-[#2E2E2E]">
         </div>
       </main>
     </>
