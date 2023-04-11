7349afa2bbd7d994029e850d6ce7cf61c3a1d29e
ItsAtanas <bogdanoskiatanas@gmail.com>
2023-04-10 23:37:17 -0400

    init

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Link Tree threeeeeeeeeeee</title>
+        <title>Link Tree threeeeeeeeeeeeeee</title>
         <meta name="description" content="My Link Tree" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
