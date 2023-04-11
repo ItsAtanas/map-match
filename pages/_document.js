e95039d103271867d8bd218fc9ea1d9fce37db0f
ItsAtanas <bogdanoskiatanas@gmail.com>
2023-04-11 10:47:28 -0400

    background

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="bg-black" lang="en">
+    <Html className="bg" lang="en">
       <Head />
       <body>
         <Main />
