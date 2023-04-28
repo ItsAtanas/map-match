56450921f501a36368fbe87324739051964bef8c
Atanas Bogdanoski <bogdanoskiatanas@gmail.com>
2023-04-28 02:37:51 -0400

    fix

--- components/Vallogic.js
+++ components/Vallogic.js
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react";
 import Image from "next/image";
+
 const Vallogic = (props) => {
   const fullStar = "/img/full-star.png"
   const emptyStar = "/img/Fmpty-Star.png"
