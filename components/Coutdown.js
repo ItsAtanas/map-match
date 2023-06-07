e35179bf80d46e5adf9be80f49d8601c27484865
Atanas Bogdanoski <bogdanoskiatanas@gmail.com>
2023-06-07 09:04:47 -0400

    dates finaly fixed

--- components/Coutdown.js
+++ components/Coutdown.js
@@ -1,29 +1,20 @@
 import { useState, useEffect } from "react";
 
+const getFutureDate = (days) => {
+  const futureDate = new Date();
+  futureDate.setDate(futureDate.getDate() + days);
+  futureDate.setHours(23, 59, 59, 999);
+  return futureDate;
+};
+
 const Countdown = () => {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [count, setCount] = useState(0);
 
-  const dates = [
-    "06/07/2023 23:59:59",
-    "06/07/2023 23:59:59",
-    "06/09/2023 23:59:59",
-    "06/10/2023 23:59:59",
-    "06/11/2023 23:59:59",
-    "06/12/2023 23:59:59",
-    "06/13/2023 23:59:59",
-    "06/14/2023 23:59:59",
-    "06/15/2023 23:59:59",
-    "06/16/2023 23:59:59",
-    "06/17/2023 23:59:59",
-    "06/18/2023 23:59:59",
-  ];
-  let data = dates[count];
-
   useEffect(() => {
-    const target = new Date(data);
+    const target = getFutureDate(count);
 
     const interval = setInterval(() => {
       const now = new Date();
@@ -58,54 +49,3 @@ const Countdown = () => {
 };
 
 export default Countdown;
-/*
-import { useState, useEffect } from "react";
-
-const Coutdown = () => {
-  const [reset, setReset] = useState(false);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  let data = "05/02/2023 23:59:59";
-
-  useEffect(() => {
-    const target = new Date(data);
-
-    const interval = setInterval(() => {
-      const now = new Date();
-      const difference = target.getTime() - now.getTime();
-
-      const h = Math.floor(
-        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      setHours(h);
-
-      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      setMinutes(m);
-
-      const s = Math.floor((difference % (1000 * 60)) / 1000);
-      setSeconds(s);
-
-      if (h <= 0 && m <= 0 && s <= 0) {
-        setReset(true);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <>
-      {reset ? (
-        <p className="text-white mt-3">Rest Me</p>
-      ) : (
-        <p className="text-white mt-3 text-3xl">
-          New Maps in: {hours}:{minutes}:{seconds}
-        </p>
-      )}
-    </>
-  );
-};
-
-export default Coutdown;
-*/
